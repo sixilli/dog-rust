bbd61516344d1fb0e6d45673d390572676a913f4
Alec <alecdude88@yahoo.com>
2019-07-02 00:58:05 -0500

    linear regression seems to work, might need further revision

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -1,94 +1,62 @@
-//pub struct LinearRegression {
-//    coef: Vec<usize>,
-//    coef_hist: Vec<usize>,
-//    output: Vec<f32>,
-//    iterations: usize,
-//    done:  bool
-//}
-//
-//impl LinearRegression {
-//    fn get_base(data: &Vec<i32>, target: &Vec<i32>) -> LinearRegression {
-//        RegressionBuilder::init().linear_regression(data, target)
-//    }
-//}
-
-//const STEP_SIZE: f64 = 1e-2;
+// Constants for basic regression properties
 const LEARNING_RATE: f64 = 1e-3;
-const MAX_ITER: usize = 2000;
+const MAX_ITER: usize = 102;
 
-pub struct RegressionBuilder {
-    //lr: f64,
-    //max_iter: usize
-}
+fn step(length: &f64, theta: &Vec<f64>, data: &Vec<f64>, target: &Vec<f64>) -> Vec<f64> {
+    let mut preds = vec![] as Vec<f64>;
+    let mut theta = vec![theta[0], theta[1]];
 
-impl RegressionBuilder {
-    // The core of the Linear Regression
-    //pub fn init() -> RegressionBuilder {
-    //    RegressionBuilder{
-    //        lr: LEARNING_RATE,
-    //        max_iter: MAX_ITER
-    //    }
-    //}
-
-    //pub fn max_iter(self,  max_iter: usize) -> RegressionBuilder {
-    //    RegressionBuilder { max_iter: max_iter, .. self}
-    //}
-
-    // Using gradient descent
-    // Steps: Start with some theta parameters,
-    //        Keep changing parameters to reduce cost
-    pub fn linear_regression(data: &Vec<f64>, target: &Vec<f64>) {
-        let m = target.len() as f64;
-        let mut coef = vec![];
-        let mut iter = 0;
-        let mut cost_hist_w = vec![0.0];
-        let mut cost_hist_b = vec![0.0];
-
-        while iter < MAX_ITER {
-            for (index, x_val) in data.iter().enumerate(){
-                let mut x = x_val;
-                let mut y = target[index];
-
-                let mut bias = 0 as f64;
-                let mut gradients = vec![] as Vec<f64>;
-                // Allow for multivirate regression
-                for grad in 0..coef.len() {
-                    gradients[grad] = 0.0;
-                }
-
-                // Performing Gradient Descent
-                // Calculating Gradient
-                for g in 0..gradients.len() {
-                    gradients[g] += -2.0 * x * (y - (gradients[g] + bias));
-                    bias += -2.0 * (y - (gradients[g] + bias));
-                }
-
-                // Updating Gradient
-                bias -= (bias / m) * LEARNING_RATE;
-                for g in 0..gradients.len() {
-                    gradients[g] -= (gradients[g] / m) as f64 * LEARNING_RATE;
-                }
-
-                for g in 0..gradients.len() {
-                    if g == 0 {
-                        coef[g] = bias;
-                    } else{
-                        coef[g] = gradients[g];
-                    }
-                }
-                cost_hist_w.push(coef[1]);
-                cost_hist_b.push(coef[0]);
-                iter += 1;
-            }
-        }
-        println!("bias: {:?}, weight: {:?}", coef[0], coef[1]);
+    for i in 0..data.len(){
+        preds.push(theta[0] + theta[1] * data[i]);
     }
 
+    let mut squared_diff = vec![] as Vec<f64>;
+    for i in 0..preds.len(){
+        squared_diff.push((preds[i] - target[i]).powi(2));
+    }
+
+    let squared_sum: f64 = squared_diff.iter().sum();
+
+    let cost = (1.0 / (2.0 * length)) * squared_sum;
+
+    //Updating theta parameters
+    theta[0] = theta[0] - LEARNING_RATE * cost;
+    theta[1] = theta[1] - LEARNING_RATE * cost;
+
+    //Creating output vec to easily access data
+    let output = vec![cost, theta[0], theta[1]];
+
+    return output
+
 }
 
-fn main() {
-    let data = vec![1f64, 2.0, 3.0];
-    let target = vec![2f64, 6.0, 4.0];
+pub fn linear_regression(data: &Vec<f64>, target: &Vec<f64>) -> Vec<f64> {
+    let m = target.len() as f64;
+    let mut coef = vec![1.0, 2.0];
+    let mut iter = 0;
+    let mut cost_hist: Vec<f64> = vec![];
 
-    RegressionBuilder::linear_regression(&data, &target);
+    while iter < MAX_ITER {
+        let output = step(&m, &coef, &data, &target);
+        coef[0] = output[1];
+        coef[1] = output[2];
+        cost_hist.push(output[0]);
+
+        if iter % 10 == 0 {
+            println!("Iter: {} Cost: {} Theta 0: {} Theta1: {}", iter, output[0], output[1], output[2]);
+            iter += 1;
+        } else {
+            iter += 1;
+        }
+
+    }
+    return coef
+}
+
+
+
+fn main() {
+    let data = vec![1f64, 2.0, 3.0, 4.0, 5.0, 6.0];
+    let target = vec![2f64, 6.0, 4.0, 10.0, 5.0, 6.0];
+    linear_regression(&data, &target);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,8 @@
-// Constants for basic regression properties
-const LEARNING_RATE: f64 = 1e-3;
-const MAX_ITER: usize = 20;
-
-fn cost_func(length: &f64, theta: &Vec<f64>, data: &Vec<f64>, target: &Vec<f64>) -> f64 {
-    let preds = vec![] as Vec<f64>;
-    for i in 0..data.len(){
-        preds.push(theta[0] + theta[1] * data[i]);
-    }
-
-    let squared_diff = vec![] as Vec<f64>;
-    for i in 0..preds.len(){
-        squared_diff.push((preds[i] - target[i]).powi(2));
-    }
-
-    let cost = (1.0 / (2.0 * length)) * squared_diff.iter().sum();
-    return cost
-
-}
-
-fn gradient_descent(theta: &Vec<f64>, data: &Vec<f64>, target: &Vec<f64>) -> Vec<f64> {
-    theta;
-}
-
-pub fn linear_regression(data: &Vec<f64>, target: &Vec<f64>) -> Vec<f64> {
-    let m = target.len() as f64;
-    let mut coef = vec![1.0, 2.0];
-    let mut iter = 0;
-    let mut cost_hist_w = vec![];
-    let mut cost_hist_b = vec![];
-
-    while iter < MAX_ITER {
-        for (x_val, y_val) in data.iter().zip(target.iter()) {
-            let x = x_val;
-            let y = y_val;
-            let mut bias = 0 as f64;
-            let mut gradients = vec![] as Vec<f64>;
-            // Allow for multivirate regression
-            for g in 0..coef.len() {
-                gradients.push(0.0);
-            }
-
-            // Performing Gradient Descent
-            // Calculating Gradient
-            for g in 0..gradients.len() {
-                gradients[g] += -2.0 * x * (y - (gradients[g] + bias));
-                bias += -2.0 * (y - (gradients[g] + bias));
-            }
-
-            // Updating Gradient
-            bias -= (bias / m) * LEARNING_RATE;
-            for g in 0..gradients.len() {
-                gradients[g] -= (gradients[g] / m) as f64 * LEARNING_RATE;
-            }
-
-            for g in 0..gradients.len() {
-                if g == 0 {
-                    coef[g] = bias;
-                } else{
-                    coef[g] = gradients[g];
-                }
-            }
-            cost_hist_w.push(coef[1]);
-            cost_hist_b.push(coef[0]);
-            iter += 1;
-            println!("bias {} weight {}", coef[0], coef[1])
-        }
-    }
-    return coef
-}
-
-
+mod linear_regression;
+use linear_regression::linear_regression;
 
 fn main() {
     let data = vec![1f64, 2.0, 3.0, 4.0, 5.0, 6.0];
     let target = vec![2f64, 6.0, 4.0, 10.0, 5.0, 6.0];
-    let reg = linear_regression(&data, &target);
-    println!("bias: {}, weight: {}", reg[0], reg[1]);
+    linear_regression(&data, &target);
 }
