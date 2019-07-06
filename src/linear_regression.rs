b9b334e2b7937feadb93689684b91968cb54fcde
Alec <alecdude88@yahoo.com>
2019-07-06 12:58:47 -0500

    Improved Speed

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -1,78 +1,55 @@
+// Big thanks to /u/leudz for feedback on properly utilizng Rust
+// Run time improved from 15ms to 2ms
 // Constants for basic regression properties
+
 const LEARNING_RATE: f64 = 1e-3;
-const MAX_ITER: usize = 1001;
+const MAX_ITER: usize = 1000;
 
-fn step(length: &f64, theta: &Vec<f64>, data: &Vec<f64>, target: &Vec<f64>) -> Vec<f64> {
-    // Intializing vectors for data.
-    let mut preds = vec![] as Vec<f64>;
-    let mut theta = vec![theta[0], theta[1]];
+fn step([mut theta0, mut theta1]: [f64; 2], data: &[f64], target: &[f64]) -> [f64; 3] {
+    let mut sum_reg_diff = 0.0;
+    let mut x_sum_reg_diff = 0.0;
 
-    // Getting predictions
-    for i in 0..data.len(){
-        preds.push(theta[0] + theta[1] * data[i]);
-    }
+    let squared_sum: f64 = data
+        .iter()
+        .zip(target.iter())
+        .map(|(&data, &target)| {
+            let pred = theta0 + theta1 * data;
 
-    // Using squared difference to calculate loss.
-    let mut squared_diff = vec![] as Vec<f64>;
-    for i in 0..preds.len(){
-        squared_diff.push((preds[i] - target[i]).powi(2));
-    }
+            sum_reg_diff += pred - target;
+            x_sum_reg_diff += (pred - target) * data;
 
-    // Sum of total loss
-    let squared_sum: f64 = squared_diff.iter().sum();
+            (pred - target).powi(2)
+        })
+        .sum();
 
-    // Finding average loss
+    let length = target.len() as f64;
     let loss = (1.0 / (2.0 * length)) * squared_sum;
 
-    // Finding difference between prediction and target
-    // Creating two different vecs as theta0 and theta1
-    // Require different approaches
-    let mut reg_diff = vec![] as Vec<f64>;
-    let mut x_reg_diff = vec! [] as Vec<f64>;
+    theta0 -= LEARNING_RATE * (1.0 / length) * sum_reg_diff;
+    theta1 -= LEARNING_RATE * (1.0 / length) * x_sum_reg_diff;
 
-    for i in 0..preds.len(){
-        reg_diff.push(preds[i] - target[i]);
-        x_reg_diff.push((preds[i] - target[i]) * data[i]);
-    }
-
-    let sum_reg_diff: f64 = reg_diff.iter().sum();
-    let x_sum_reg_diff: f64 = x_reg_diff.iter().sum();
-
-    //Applying gradient descent to theta parameters
-    theta[0] = theta[0] - LEARNING_RATE  * (1.0 / length) * sum_reg_diff;
-    theta[1] = theta[1] - LEARNING_RATE  * (1.0 / length) * x_sum_reg_diff;
-
-    //Creating output vec to easily access data
-    let output = vec![loss, theta[0], theta[1]];
-
-    return output
-
+    [loss, theta0, theta1]
 }
 
-pub fn linear_regression(data: &Vec<f64>, target: &Vec<f64>) -> Vec<f64> {
-    // Creating parameters for running regression
-    let m = target.len() as f64;
+pub fn linear_regression(data: &[f64], target: &[f64]) -> Vec<f64> {
     let mut coef = vec![1.0, 2.0];
-    let mut iter = 0;
-    let mut loss_hist: Vec<f64> = vec![];
+    let mut loss_hist = Vec::with_capacity(MAX_ITER);
 
-    // Running model until max iter is reached
-    while iter < MAX_ITER {
-        let output = step(&m, &coef, &data, &target);
+    for iter in 0..=MAX_ITER {
+        let [loss, theta0, theta1] = step([coef[0], coef[1]], &data, &target);
 
         // Updating coefficients and apending to loss history
-        coef[0] = output[1];
-        coef[1] = output[2];
-        loss_hist.push(output[0]);
+        coef[0] = theta0;
+        coef[1] = theta1;
+        loss_hist.push(loss);
 
-        // For debugging purposes, print some metrics every 10 steps
+        // For debugging purposes, print some metrics every 100 steps
         if iter % 100 == 0 {
-            println!("Iter: {} Cost: {} Theta 0: {} Theta1: {}", iter, output[0], output[1], output[2]);
-            iter += 1;
-        } else {
-            iter += 1;
+            println!(
+                "Iter: {} Cost: {} Theta 0: {} Theta1: {}",
+                iter, loss, theta0, theta1
+            );
         }
-
     }
-    return coef
+    coef
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::time::Instant;
+
 mod linear_regression;
 use linear_regression::linear_regression;
 
 fn main() {
+    let now = Instant::now();
     let data = vec![1f64, 2.0, 3.0, 4.0, 5.0, 6.0];
     let target = vec![2f64, 6.0, 4.0, 10.0, 5.0, 6.0];
     linear_regression(&data, &target);
+    let new_now = Instant::now();
+    println!("{:?}", new_now.duration_since(now));
 }
